c5ee70cec160da471c3e22ac2978e98a3a3b2505
hakodio <hakodate.1106v@gmail.com>
2023-12-11 23:52:51 +0900

    #2 コンポーネントのインポートとエクスポート、レンダー：実装

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import './styles.css'
 import { Box } from '@mui/material'
+import Title from './components/Title'
+import Border7 from './Border7'
 
 /**
  * Appコンポーネント
@@ -12,8 +14,8 @@ import { Box } from '@mui/material'
 export default function App() {
   return (
     <Box className="app">
-      {/* Title */}
-      {/* Border7 */}
+      <Title />
+      <Border7 />
     </Box>
   )
 }
